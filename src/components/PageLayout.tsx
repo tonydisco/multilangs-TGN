529e9e4a076643ce02673a8b082860a2b7150e3e
Tony <86345701+tonydisco@users.noreply.github.com>
2025-03-30 08:23:41 +0700

    updated image png instaed svg

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -23,7 +23,7 @@ export default function PageLayout({children, title}: Readonly<Props>) {
               width: '100%'
             }}
           >
-            <PureImage url="/landing/HERO-GRID.svg" />
+            <PureImage url="/landing/HERO-GRID.png" />
           </div>
           <div>
             <div
@@ -60,7 +60,7 @@ export default function PageLayout({children, title}: Readonly<Props>) {
               </div>
             </div>
             <div>
-              <PureImage url="/landing/HERO-BANNER.svg" />
+              <PureImage url="/landing/HERO-BANNER.png" />
             </div>
           </div>
         </div>
@@ -68,7 +68,7 @@ export default function PageLayout({children, title}: Readonly<Props>) {
       <section className="about-us">
         <div
           style={{
-            background: 'url(/landing/ABOUT-US-BD.svg)',
+            background: 'url(/landing/ABOUT-US-BD.png)',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
@@ -79,7 +79,13 @@ export default function PageLayout({children, title}: Readonly<Props>) {
           }}
         >
           <div className="flex justify-center items-center mx-auto p-24 max-w-6xl">
-            <div className="rounded-[10px] bg-browMain p-8 text-white">
+            <div
+              className="rounded-[24px]  p-8 text-white"
+              style={{
+                backgroundColor: 'rgba(110, 62, 47, 0.8)',
+                backdropFilter: 'blur(3px)'
+              }}
+            >
               <h2 className="text-center text-5xl font-bold mb-6">
                 Về chúng tôi
               </h2>
@@ -101,13 +107,13 @@ export default function PageLayout({children, title}: Readonly<Props>) {
                 </p>
                 <p>
                   {`Với sứ mệnh "Sự hài lòng của bạn là thước đo sự tồn tại và
-                  phát triển của chúng tôi”, TGN group luôn phấn đấu không ngừng
-                  để hướng đến mục tiêu chung là sự hài lòng của Quý Khách hàng.
-                  TGN group đã xây dựng một hệ thống gồm 3 trụ sở văn phòng, 26
-                  nhà máy sản xuất trải dài khắp các vùng kinh tế trọng điểm
-                  phía Nam, cùng đội ngũ hơn 1.500 cán bộ - nhân viên không
-                  ngừng làm việc và cống hiến, chúng tôi tự tin vào chất lượng
-                  sản phẩm và dịch vụ của mình.`}
+                phát triển của chúng tôi”, TGN group luôn phấn đấu không ngừng
+                để hướng đến mục tiêu chung là sự hài lòng của Quý Khách hàng.
+                TGN group đã xây dựng một hệ thống gồm 3 trụ sở văn phòng, 26
+                nhà máy sản xuất trải dài khắp các vùng kinh tế trọng điểm
+                phía Nam, cùng đội ngũ hơn 1.500 cán bộ - nhân viên không
+                ngừng làm việc và cống hiến, chúng tôi tự tin vào chất lượng
+                sản phẩm và dịch vụ của mình.`}
                 </p>
               </div>
             </div>
@@ -135,13 +141,13 @@ export default function PageLayout({children, title}: Readonly<Props>) {
               width: '100%'
             }}
           >
-            <PureImage url="/landing/GRID-VISION.svg" />
+            <PureImage url="/landing/GRID-VISION.png" />
           </div>
           <div className="flex justify-center  mx-auto p-10 max-w-6xl gap-5 item-stretch">
             <div
               className="rounded-[24px] bg-browMain p-4 text-white flex-1 relative"
               style={{
-                background: 'url(/landing/VISION.svg)',
+                background: 'url(/landing/VISION.png)',
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center'
@@ -178,7 +184,7 @@ export default function PageLayout({children, title}: Readonly<Props>) {
             <div
               className="rounded-[24px] bg-browMain p-4 text-white flex-1 relative"
               style={{
-                background: 'url(/landing/MISSION.svg)',
+                background: 'url(/landing/MISSION.png)',
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center'

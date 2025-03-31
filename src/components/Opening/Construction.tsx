e713b55ac1af7519ec562952b1ff06d05d682b9d
Tony <86345701+tonydisco@users.noreply.github.com>
2025-04-01 00:17:53 +0700

    updated landing page

---
+++ src/components/Opening/Construction.tsx
@@ -0,0 +1,269 @@
+import React, {Fragment} from 'react';
+import {PureImage} from '../Common/Images';
+import Link from 'next/link';
+
+const Construction = () => {
+  return (
+    <section
+      style={{
+        backgroundColor: '#EDDFD8',
+        position: 'relative',
+        paddingBottom: 150,
+        height: 1500
+      }}
+    >
+      <div
+        style={{
+          position: 'relative',
+          width: '100%'
+        }}
+        className="app-container"
+      >
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            position: 'relative',
+            height: 500,
+            gap: 50
+          }}
+        >
+          <div
+            style={{
+              flex: 1,
+              position: 'relative',
+              height: '100%'
+            }}
+          >
+            <div
+              style={{
+                width: 740,
+                position: 'absolute',
+                right: 120,
+                zIndex: 0,
+                top: -90
+              }}
+            >
+              <PureImage url="/landing/CONSTRUCTION/MACHINE.png" />
+            </div>
+            <div
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                zIndex: 1,
+                width: 700,
+                right: 15
+              }}
+            >
+              <PureImage url="/landing/CONSTRUCTION/NHA.png" />
+            </div>
+            <div
+              style={{
+                position: 'absolute',
+                bottom: -40,
+                zIndex: 2,
+                width: 550,
+                right: 320
+              }}
+            >
+              <PureImage url="/landing/CONSTRUCTION/XE.png" />
+            </div>
+          </div>
+          <div
+            style={{
+              flex: 1,
+              position: 'relative',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column'
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 20,
+                alignItems: 'flex-start',
+                marginLeft: 150
+              }}
+            >
+              <h2
+                style={{
+                  fontWeight: '700',
+                  fontSize: '32px',
+                  textTransform: 'uppercase'
+                }}
+              >
+                Lĩnh Vực Sản xuất
+              </h2>
+              <p style={{fontSize: 14}}>
+                <strong>Thế Giới Nhà Group</strong> sở hữu hệ thống nhà máy hiện
+                đại, sản xuất đa dạng vật liệu xây dựng đạt tiêu chuẩn chất
+                lượng cao, đáp ứng nhu cầu cho mọi công trình.
+              </p>
+              <button className="app-btn-default" style={{marginTop: 50}}>
+                <Link href="/about-us">
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      gap: 8
+                    }}
+                  >
+                    <span
+                      style={{
+                        fontSize: 14,
+                        fontWeight: 500
+                      }}
+                    >
+                      Xem thêm
+                    </span>
+                    <PureImage
+                      style={{width: 12}}
+                      url="/landing/ICON-ARROW.svg"
+                    />
+                  </div>
+                </Link>
+              </button>
+            </div>
+            <div
+              style={{
+                width: '100%',
+                position: 'absolute',
+                bottom: 0,
+                transform: 'rotateX(180deg)'
+              }}
+            >
+              <PureImage url="/landing/BUSINESS-PHILOSOPHY/BUSINESS-TEXTURE-1.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: '#E8E4E3',
+          height: 480,
+          width: '100%',
+          position: 'relative'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            gap: 50
+          }}
+          className="app-container"
+        >
+          <div
+            style={{
+              position: 'relative',
+              flex: 1,
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 20,
+                alignItems: 'flex-start'
+              }}
+            >
+              <h2
+                style={{
+                  fontWeight: '700',
+                  fontSize: '32px',
+                  textTransform: 'uppercase'
+                }}
+              >
+                Lĩnh Vực kinh doanh
+              </h2>
+              <p style={{fontSize: 14}}>
+                <strong>Thế Giới Nhà Group</strong> vận hành hệ thống Siêu thị
+                vật liệu xây dựng quy mô lớn, cung cấp giải pháp toàn diện với
+                danh mục sản phẩm phong phú từ thô đến hoàn thiện.
+              </p>
+              <button className="app-btn-default" style={{marginTop: 50}}>
+                <Link href="/about-us">
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      gap: 8
+                    }}
+                  >
+                    <span
+                      style={{
+                        fontSize: 14,
+                        fontWeight: 500
+                      }}
+                    >
+                      Xem thêm
+                    </span>
+                    <PureImage
+                      style={{width: 12}}
+                      url="/landing/ICON-ARROW.svg"
+                    />
+                  </div>
+                </Link>
+              </button>
+            </div>
+            <div
+              style={{
+                width: '100%',
+                position: 'absolute',
+                bottom: 0,
+                transform: 'rotateX(180deg)'
+              }}
+            >
+              <PureImage url="/landing/BUSINESS-PHILOSOPHY/BUSINESS-TEXTURE-1.png" />
+            </div>
+          </div>
+          <div
+            style={{
+              flex: 1,
+              height: '100%'
+            }}
+            // style={{
+            //   flex: 1,
+            //   background:
+            //     'url(/landing/CONSTRUCTION/SHOW-ROOM.png) no-repeat center',
+            //   backgroundSize: 'contain',
+            //   height: '100%',
+            //   position: 'relative'
+            // }}
+          >
+            <div
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                right: 0,
+                zIndex: 1,
+                height: 400
+              }}
+            >
+              <PureImage
+                url="/landing/CONSTRUCTION/SHOW-ROOM.png"
+                mode="cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>block 3</div>
+    </section>
+  );
+};
+
+export default Construction;
---
+++ src/components/Opening/Introduce.tsx
@@ -0,0 +1,203 @@
+import React, {Fragment} from 'react';
+import {PureImage} from '../Common/Images';
+import Link from 'next/link';
+
+const Introduce = () => {
+  return (
+    <section>
+      <div
+        style={{
+          backgroundColor: '#FAFAFA',
+          position: 'relative',
+          overflow: 'hidden',
+          paddingBottom: 150
+        }}
+      >
+        <div
+          className="app-container"
+          style={{
+            padding: '150px 0'
+          }}
+        >
+          <div className="base-card">
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                gap: 40,
+                alignItems: 'center',
+                padding: '15px 25px'
+              }}
+            >
+              <div style={{flex: 1}}>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 20,
+                    alignItems: 'flex-start'
+                  }}
+                >
+                  <h2
+                    style={{
+                      fontWeight: '700',
+                      fontSize: '32px',
+                      textTransform: 'uppercase'
+                    }}
+                  >
+                    giới thiệu
+                  </h2>
+                  <p style={{fontSize: 14}}>
+                    <strong>Thế Giới Nhà Group</strong> hệ thống Siêu thị vật
+                    liệu xây dựng hàng đầu, cung cấp giải pháp toàn diện cho mọi
+                    công trình.
+                  </p>
+                  <button className="app-btn-default">
+                    <Link href="/about-us">
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                          alignItems: 'center',
+                          gap: 8
+                        }}
+                      >
+                        <span
+                          style={{
+                            fontSize: 14,
+                            fontWeight: 500
+                          }}
+                        >
+                          Về chúng tôi
+                        </span>
+                        <PureImage
+                          style={{width: 12}}
+                          url="/landing/ICON-ARROW.svg"
+                        />
+                      </div>
+                    </Link>
+                  </button>
+                </div>
+              </div>
+              <div style={{flex: 1.5}}>
+                <div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'flex-end',
+                      gap: 30
+                    }}
+                  >
+                    {statistics.map((item, index) => {
+                      return (
+                        <Fragment key={index}>
+                          <div
+                            style={{
+                              flex: 1,
+                              display: 'flex',
+                              flexDirection: 'column',
+                              alignItems: 'center',
+                              textAlign: 'center'
+                            }}
+                          >
+                            <PureImage
+                              url={item.icon}
+                              style={{width: 50, height: '50px'}}
+                            />
+                            <div style={{marginTop: 20}}>
+                              <h2
+                                style={{
+                                  fontSize: 32,
+                                  fontWeight: 700,
+                                  color: '#925846'
+                                }}
+                              >
+                                {item.title}
+                              </h2>
+                              <p style={{fontSize: 14}}>{item.sub}</p>
+                            </div>
+                          </div>
+                          {index !== statistics.length - 1 && (
+                            <div className="default-vertical-line" />
+                          )}
+                        </Fragment>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="app-container">
+          <div
+            style={{
+              position: 'relative',
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <div
+              className="base-video"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <iframe
+                src="https://www.youtube.com/embed/AJFkU1bC3C0?si=ZmPXd6J_k3iaOqPa"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+                style={{
+                  position: 'absolute',
+                  height: 500 - 24,
+                  borderRadius: 24,
+                  width: 800 - 24,
+                  zIndex: 1
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            width: '100%'
+          }}
+        >
+          <PureImage url="/landing/VIDEO-GRID.png" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Introduce;
+
+const statistics = [
+  {
+    title: '16+',
+    sub: 'Năm đồng hành và phát triển',
+    icon: '/landing/ICON-INTRO-BUILDING.svg'
+  },
+  {
+    title: '1500+',
+    sub: 'Nhân sự',
+    icon: '/landing/ICON-INTRO-PEOPLE.svg'
+  },
+  {
+    title: '26',
+    sub: 'Nhà máy',
+    icon: '/landing/ICON-INTRO-FACTORY.svg'
+  },
+  {
+    title: '03',
+    sub: 'Văn phòng',
+    icon: '/landing/ICON-INTRO-OFFICE.svg'
+  }
+];

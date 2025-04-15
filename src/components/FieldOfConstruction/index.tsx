a8e858578d3beb6d6acd12e3de48f2fab675375f
tonydisco <ngotantai.uit@gmail.com>
2025-04-15 13:42:57 +0700

    updated lags

--- src/components/FieldOfConstruction/index.tsx
+++ src/components/FieldOfConstruction/index.tsx
@@ -1,9 +1,16 @@
+'use client';
 import React from 'react';
 import FieldOfBusiness from './FieldOfBusiness';
 import FieldOfProduction from './FieldOfProduction';
 import FieldOfExtraction from './FieldOfExtraction';
+import {useAppContext} from '@/Providers';
 
 const FieldOfConstruction = () => {
+  const {locale} = useAppContext();
+  console.log('====================================');
+  console.log({locale});
+  console.log('====================================');
+
   return (
     <>
       <FieldOfProduction />

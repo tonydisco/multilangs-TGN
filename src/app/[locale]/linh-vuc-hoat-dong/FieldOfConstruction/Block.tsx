c0cfe0951b2cb6d66e4a0bed03b1d5962e250b62
HungHd <hoangkyo992@gmail.com>
2025-04-20 10:16:17 +0700

    Update

--- src/app/[locale]/linh-vuc-hoat-dong/FieldOfConstruction/Block.tsx
+++ src/app/[locale]/linh-vuc-hoat-dong/FieldOfConstruction/Block.tsx
@@ -1,74 +1,21 @@
 import React from 'react';
-import {PureImage} from '../../../../components/Common/Images';
 import parse from 'html-react-parser';
-import Link from 'next/link';
+import {CardBase} from '@/components/Common/Card';
 
 const Block = ({attribs, content}: any) => {
   const item = content?.contents?.find((x: any) => x.language == 'vi') ?? {};
-  const block = item.blocks.length > 0 ? item.blocks[0] : {};
-  const html = parse(block.content, {
-    replace: (domNode: any) => {
-      if (domNode.attribs && domNode.attribs['auto-block-code']) {
-        // const blockCode = domNode.attribs['auto-block-code'];
-        return domNode;
-      }
-      return domNode;
-    }
-  });
+  const excerpt = parse(item.excerpt, {});
   return (
     <div className={attribs.class} id={attribs.id}>
-      <div
-        className="base-card-full-border base-card-full-border-fop"
-        style={{
-          padding: 24,
-          position: 'relative',
-          height: 340
+      <CardBase
+        item={{
+          title: item.title,
+          content: excerpt,
+          image: content.featuredImageUrl,
+          linkTo: content.slug,
+          btnText: 'Xem chi tiết'
         }}
-      >
-        <div className="d-flex align-items-stretch h-100 gap-3">
-          <div className="field-of-production-item__content h-100 w-100">
-            <div className="d-flex flex-column justify-content-between h-100">
-              <div>
-                <h3 className="tgn-box-title text-start" style={{fontSize: 22}}>
-                  {item.title}
-                </h3>
-                <div
-                  style={{
-                    color: '#616161',
-                    marginTop: 20
-                  }}
-                  className="text-start fs-12px md:fs-14px"
-                >
-                  {html}
-                </div>
-              </div>
-              <div className="text-start">
-                <button className="app-btn-default">
-                  <Link href={`/${content.slug}`}>
-                    <div className="app-btn-default-flex">
-                      <span style={{fontWeight: 500}}>Xem chi tiết</span>
-                      <PureImage
-                        style={{width: 12}}
-                        url="/landing/ICON-ARROW.svg"
-                      />
-                    </div>
-                  </Link>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="field-of-production-item__image w-100">
-            <div className="w-100 h-100 overflow-hidden rounded-4">
-              <PureImage
-                url={content.featuredImageUrl}
-                style={{
-                  objectFit: 'cover'
-                }}
-              ></PureImage>
-            </div>
-          </div>
-        </div>
-      </div>
+      ></CardBase>
     </div>
   );
 };
--- src/app/[locale]/linh-vuc-hoat-dong/FieldOfConstruction/index.tsx
+++ src/app/[locale]/linh-vuc-hoat-dong/FieldOfConstruction/index.tsx
@@ -1,8 +1,5 @@
 'use client';
 import React from 'react';
-import FieldOfBusiness from './FieldOfBusiness';
-import FieldOfProduction from './FieldOfProduction';
-import FieldOfExtraction from './FieldOfExtraction';
 import {useAppContext} from '@/Providers';
 import parse from 'html-react-parser';
 import Block from '@/app/[locale]/linh-vuc-hoat-dong/FieldOfConstruction/Block';
@@ -16,27 +13,19 @@ const FieldOfConstruction = ({pageContent}: any) => {
       {content.blocks?.map((block: any, index: number) => {
         const html = parse(block.content, {
           replace: (domNode: any) => {
-            if (domNode.attribs && domNode.attribs['auto-block-code']) {
-              const blockCode = domNode.attribs['auto-block-code'];
-              return (
-                <Block
-                  attribs={domNode.attribs}
-                  content={pageContent.children.find(
-                    (x: any) =>
-                      x.slug === blockCode ||
-                      x.slug == 'san-xuat-be-tong-thuong-pham'
-                  )}
-                />
-              );
+            if (domNode.attribs && domNode.attribs['data-auto-block-code']) {
+              const blockCode = domNode.attribs['data-auto-block-code'];
+              const arr = blockCode.split('](');
+              // const code = arr[0].replace(']', '');
+              const slug = arr[1].replace(')', '');
+              const b = pageContent.children.find((x: any) => x.slug == slug);
+              return b && <Block attribs={domNode.attribs} content={b} />;
             }
             return domNode;
           }
         });
         return <div key={index}>{html}</div>;
       })}
-      <FieldOfProduction />
-      <FieldOfBusiness />
-      <FieldOfExtraction />
     </>
   );
 };

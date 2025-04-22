7333e6507b048a804ffb488e3de67f8a44abd8ae
HungHd <hoangkyo992@gmail.com>
2025-04-22 22:01:58 +0700

    Update content + translate

--- src/app/[locale]/linh-vuc-hoat-dong/ChildPage.tsx
+++ src/app/[locale]/linh-vuc-hoat-dong/ChildPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import parse from 'html-react-parser';
 import {CardBase} from '@/components/Common/Card';
+import {getTranslations} from 'next-intl/server';
 
-const Block = ({attribs, content}: any) => {
-  const item = content?.contents?.find((x: any) => x.language == 'vi') ?? {};
+const ChildPage = async ({attribs, content, locale}: any) => {
+  const t = await getTranslations();
+  const item = content?.contents?.find((x: any) => x.language == locale) ?? {};
   const excerpt = parse(item.excerpt, {});
   return (
     <div className={attribs.class} id={attribs.id}>
@@ -13,11 +15,11 @@ const Block = ({attribs, content}: any) => {
           content: excerpt,
           image: content.featuredImageUrl,
           linkTo: content.slug,
-          btnText: 'Xem chi tiết'
+          btnText: t('ViewDetail')
         }}
       ></CardBase>
     </div>
   );
 };
 
-export default Block;
+export default ChildPage;
--- src/app/[locale]/linh-vuc-hoat-dong/page.tsx
+++ src/app/[locale]/linh-vuc-hoat-dong/page.tsx
@@ -1,18 +1,45 @@
 import {TitlePageView} from '@/components/Hero';
 import PageLayout from '@/components/PageLayout';
-import FieldOfConstruction from './FieldOfConstruction';
+import ChildPage from './ChildPage';
 import {getPage} from '@/apis/pages';
+import parse from 'html-react-parser';
 
 export default async function Page({params}: any) {
   const {locale} = await params;
-  const getPageResult = await getPage('linh-vuc-hoat-dong');
-  const content = getPageResult.contents.find((x: any) => x.language == locale);
+  const pageSlug = 'linh-vuc-hoat-dong';
+  const pageContent = await getPage(pageSlug);
+  const content = pageContent.contents.find((x: any) => x.language == locale);
   return (
     <PageLayout
       title={<TitlePageView title={content?.title} />}
-      className="lvhd-wrapper"
+      className="page-wrapper"
     >
-      <FieldOfConstruction pageContent={getPageResult} />
+      {content.blocks?.map((block: any, index: number) => {
+        const html = parse(block.content, {
+          replace: (domNode: any) => {
+            if (domNode.attribs && domNode.attribs['data-auto-block-code']) {
+              const blockCode = domNode.attribs['data-auto-block-code'];
+              const arr = blockCode.split('](');
+              // const code = arr[0].replace(']', '');
+              const slug = arr[1].replace(')', '');
+              const childContent = pageContent.children.find(
+                (x: any) => x.slug == slug
+              );
+              return (
+                childContent && (
+                  <ChildPage
+                    attribs={domNode.attribs}
+                    content={childContent}
+                    locale={locale}
+                  />
+                )
+              );
+            }
+            return domNode;
+          }
+        });
+        return <div key={index}>{html}</div>;
+      })}
     </PageLayout>
   );
 }

9e616a00601d321356fb405d239f0c575f1be5bd
tonydisco <ngotantai.uit@gmail.com>
2025-04-28 11:16:49 +0700

    term and policy page updated

--- src/app/[locale]/dieu-khoan-su-dung/page.tsx
+++ src/app/[locale]/dieu-khoan-su-dung/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import {getPage} from '@/apis/pages';
+import {SectionTitles} from '@/components/Common/Titles';
+import {IPageDefaultProps} from '@/models/interface';
+import NotFoundPage from '../not-found';
+import {pageBySlug} from '@/utils/config';
 
-const Page = () => {
+const Page = async ({params}: Readonly<IPageDefaultProps>) => {
+  const {locale} = await params;
+  const pageData = await getPage(pageBySlug.term);
+
+  const findContentByLocale = pageData?.contents?.find(
+    (item: any) => item.language === locale
+  );
+
   return (
-    <div style={{height: 500}}>
-      <div className="container">dieu-khoan-su-dung</div>
+    <div className="tgn-terms-of-use-page">
+      <div className="container">
+        <div style={{padding: '75px 0 150px 0'}}>
+          {(() => {
+            if (findContentByLocale?.blocks?.length > 0) {
+              return (
+                <div>
+                  <SectionTitles
+                    style={{textAlign: 'center', padding: '25px 0'}}
+                    title={findContentByLocale?.title}
+                  />
+                  {findContentByLocale.blocks.map((item: any) => {
+                    return (
+                      <div
+                        key={item.id}
+                        className="tgn-terms-of-use-block"
+                        dangerouslySetInnerHTML={{__html: item.content}}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            }
+            return <NotFoundPage />;
+          })()}
+        </div>
+      </div>
     </div>
   );
 };

92b0050b464f447525f91958c0e95a4d15a3b948
Tony <86345701+tonydisco@users.noreply.github.com>
2025-04-28 07:11:02 +0700

    page created

---
+++ src/app/[locale]/dieu-khoan-su-dung/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div style={{height: 500}}>
+      <div className="container">dieu-khoan-su-dung</div>
+    </div>
+  );
+};
+
+export default Page;

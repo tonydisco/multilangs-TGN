4a9a86df76a4ccfa498218de80b0d3bdef7b8fd5
tonydisco <ngotantai.uit@gmail.com>
2025-05-04 12:40:17 +0700

    roadmap from api  updated

---
+++ src/apis/roadmap.ts
@@ -0,0 +1,29 @@
+import {apiRequest} from '@/services/fetchAPI';
+
+interface IGetRoadMap {
+  page: number;
+  pageSize: number;
+  typeId: string;
+  categories: string;
+  orderBy: string;
+  direction: string;
+}
+
+const defaultQuery = {
+  page: 1,
+  pageSize: 1000,
+  typeId: 'event',
+  categories: 'DevelopmentStages',
+  orderBy: 'title',
+  direction: 'asc'
+};
+
+export const getRoadMap = async (
+  _query: Partial<IGetRoadMap> = defaultQuery
+) => {
+  return (
+    await apiRequest<any>(`public/posts`, {
+      params: {...defaultQuery, ..._query}
+    })
+  ).result;
+};
--- src/app/[locale]/gioi-thieu/page.tsx
+++ src/app/[locale]/gioi-thieu/page.tsx
@@ -4,16 +4,19 @@ import PageLayout from '@/components/PageLayout';
 import {IPageDefaultProps} from '@/models/interface';
 import {getTranslations} from 'next-intl/server';
 import '@/styles/aboutUs.scss';
+import {getRoadMap} from '@/apis/roadmap';
 
 export default async function Page({params}: Readonly<IPageDefaultProps>) {
   const {locale} = await params;
   const t = await getTranslations({locale});
+  const roadMap = await getRoadMap();
+
   return (
     <PageLayout
       title={<HeaderTitleView text={t('Slogan')} />}
       className="about-us-wrapper"
     >
-      <AboutUsView />
+      <AboutUsView roadMap={roadMap} />
     </PageLayout>
   );
 }

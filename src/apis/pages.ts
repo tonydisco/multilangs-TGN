d1b67b1d2f1ce55e290a368045af77f070be3206
tonydisco <ngotantai.uit@gmail.com>
2025-05-08 09:36:32 +0700

    updated news details

--- src/apis/pages.ts
+++ src/apis/pages.ts
@@ -1,5 +1,8 @@
 import {apiRequest} from '@/services/fetchAPI';
 
 export const getPage = async (slug: string) => {
+  if (!slug) {
+    return null;
+  }
   return (await apiRequest<any>(`public/posts/contents?slug=${slug}`)).result;
 };
